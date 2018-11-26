37b5177bb349e77509e6e67a77ffc34cf8f742ef
Steven Kussmaul <steve@Stevens-MacBook-Air.local>
2018-11-26 10:34:38 -0500

    initial commit to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", cartList);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`server is on port ${port}`);
 });
