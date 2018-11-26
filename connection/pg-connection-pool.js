0e52d94eb8101bb0a22bbbfd346b7b4a4328d00c
Steven Kussmaul <steve@Stevens-MacBook-Air.local>
2018-11-26 10:58:43 -0500

    heroku database update

--- connection/pg-connection-pool.js
+++ connection/pg-connection-pool.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const pg = require("pg");
-const pool = {
-    user: 'postgres',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
-    database: 'ExpressShopDB',
-    ssl: false
+const url = require("url");
+try {
+require("dotenv").config();
+} catch (e) {
+console.log(e);
 }
-
-module.exports = new pg.Pool(pool);
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+const config = {
+user: auth[0],
+password: auth[1],
+host: params.hostname,
+port: params.port,
+database: params.pathname.split("/")[1],
+ssl: params.hostname !== "localhost"
+};
+module.exports = new pg.Pool(config);
